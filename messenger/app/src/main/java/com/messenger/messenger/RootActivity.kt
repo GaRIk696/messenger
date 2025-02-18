943fc97cfc0a4b73717dcc111e1231f45f185e00
GaRIk696 <vikingkadet21@yandex.ru>
2025-02-19 00:33:18 +0300

    first update

---
+++ messenger/app/src/main/java/com/messenger/messenger/RootActivity.kt
@@ -0,0 +1,24 @@
+package com.messenger.messenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.messenger.messenger.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
