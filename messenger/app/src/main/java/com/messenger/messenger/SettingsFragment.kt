46cda60cdbde6d4a274cb953b1559ebcc18c32ab
GaRIk696 <vikingkadet21@yandex.ru>
2025-02-24 10:24:50 +0300

    0.7.1

--- messenger/app/src/main/java/com/messenger/messenger/SettingsFragment.kt
+++ messenger/app/src/main/java/com/messenger/messenger/SettingsFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.messenger.messenger.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
@@ -17,11 +20,27 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
-
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val themeSwitcher: SwitchMaterial = binding.themeSwitcher
+
+        val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+        themeSwitcher.isChecked = currentNightMode == AppCompatDelegate.MODE_NIGHT_YES
+
+        themeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
