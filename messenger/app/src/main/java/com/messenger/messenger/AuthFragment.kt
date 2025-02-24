3345e95e64f2194aa545e2512d3bcf1c7ee6b76b
GaRIk696 <vikingkadet21@yandex.ru>
2025-02-24 10:37:07 +0300

    0.7.2

--- messenger/app/src/main/java/com/messenger/messenger/AuthFragment.kt
+++ messenger/app/src/main/java/com/messenger/messenger/AuthFragment.kt
@@ -10,10 +10,8 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.messenger.messenger.databinding.FragmentAuthBinding
 
 class AuthPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    // Указываем количество элементов, которые будут отображаться в ViewPager
     override fun getItemCount(): Int = 2
 
-    // Возвращаем фрагменты для каждой позиции
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> LoginFragment()
@@ -27,7 +25,6 @@ class AuthFragment : Fragment() {
     private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
 
-    // Добавляем переменную для TabLayoutMediator
     private lateinit var tabsMediator: TabLayoutMediator
 
     override fun onCreateView(
@@ -41,29 +38,23 @@ class AuthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Привязываем созданный ранее адаптер к ViewPager
         binding.viewPager.adapter = AuthPagerAdapter(this)
 
-        // Создаем TabLayoutMediator
         tabsMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            // Возвращаем желаемое название для каждой позиции
             when (position) {
                 0 -> tab.text = "Войти"
                 1 -> tab.text = "Регистрация"
             }
         }
 
-        // Привязываем созданный TabLayoutMediator к фрагменту
         tabsMediator.attach()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
 
-        // Отвязываем TabLayoutMediator при уничтожении фрагмента
         tabsMediator.detach()
 
-        // Очищаем binding
         _binding = null
     }
 }
